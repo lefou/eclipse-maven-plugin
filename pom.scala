bab72c166fd07b67b3d23d18a2b74ff27304e504
Tobias Roeser <le.petit.fou@web.de>
2018-03-12 10:47:21 +0100

    Prepared release 0.0.3

--- pom.scala
+++ pom.scala
@@ -2,7 +2,7 @@ import org.sonatype.maven.polyglot.scala.model._
 import scala.collection.immutable._
 
 val projectName = "eclipse-maven-plugin"
-val gav = "de.tototec" % s"de.tobiasroeser.${projectName}" % "0.0.3-SNAPSHOT"
+val gav = "de.tototec" % s"de.tobiasroeser.${projectName}" % "0.0.3"
 val url = "https://github.com/lefou/eclipse-maven-plugin"
 
 implicit class RichDependency(d: Dependency) {
